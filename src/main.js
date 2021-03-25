1937e8409f9f0f072345c6aaac70e91340593803
lexx <lexx@localhost>
2021-03-25 12:06:14 +0200

    add analytics

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import wb from './registerServiceWorker'
 import './assets/tailwind.css'
+import firebase from 'firebase/app'
+import 'firebase/analytics'
+import appConfig from '../firebase.app.json'
 
+// if (process.env.NODE_ENV === 'production') {
+
+// }
 const app = createApp(App)
 app.config.globalProperties.$workbox = wb
 app.mount('#app')
+
+firebase.initializeApp(appConfig)
+firebase.analytics()
