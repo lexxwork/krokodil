af3a87a80b00a1a29fe8e5fe1e6fbc8c604a6695
lexx <lexx@localhost>
2021-09-18 16:31:12 +0300

    update eslint settings and package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    es2020: true
   },
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/standard'
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 11
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': ['error', {'code': 120}]
   }
 }
